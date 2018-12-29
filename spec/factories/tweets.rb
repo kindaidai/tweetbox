73f731e26b0fb261289ba1eacccdbc1ca3ceb47c
kindaichidai <kindai.0911@gmail.com>
2018-12-29 11:12:29 +0900

    fix /lib/assets/jabascripts/navbar.js and execute annotate

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: tweets
+#
+#  id         :bigint(8)        not null, primary key
+#  user_id    :bigint(8)        not null
+#  tweet_id   :bigint(8)        not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryBot.define do
   factory :tweet do
     
